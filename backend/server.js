215f7f8c662e97cea81b09480b3cac438ca7fbea
Abhiii0102 <abhiii0102@gmail.com>
2026-04-01 14:52:45 +0530

    gg8

--- backend/server.js
+++ backend/server.js
@@ -31,7 +31,9 @@ const Contact = mongoose.model("Contact", contactSchema);
 
 // ================= EMAIL SETUP =================
 const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: "smtp.office365.com",
+  port: 587,
+  secure: false,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
